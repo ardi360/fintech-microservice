c4d925e05867d99cabb7493ff0a9c49971c5db00
ardalan zahiri <ardalanzahiri@gmail.com>
2024-06-26 15:46:12 +0330

    added main business service for future working

---
+++ banking-service/src/main/java/ir/snapppay/bankingservice/aspect/LoggingAspect.java
@@ -0,0 +1,5 @@
+package ir.snapppay.bankingservice.aspect;
+
+//todo : add @Around aspects to log around incoming and outgoing requests
+public class LoggingAspect {
+}
---
+++ banking-service/src/main/java/ir/snapppay/bankingservice/controller/BankingServicesController.java
@@ -0,0 +1,10 @@
+package ir.snapppay.bankingservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BankingServicesController {
+    //todo : dont forget to add swagger for api documenting as per requested later after all things done!
+}
