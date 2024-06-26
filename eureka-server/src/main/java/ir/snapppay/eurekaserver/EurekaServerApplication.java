9cd58f8e28a24340cb1a7008934a3fb4566190fb
ardalan zahiri <ardalanzahiri@gmail.com>
2024-06-26 14:01:36 +0330

    config server added

--- eureka-server/src/main/java/ir/snapppay/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ir/snapppay/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package ir.snappay.eurekaserver;
+package ir.snapppay.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
