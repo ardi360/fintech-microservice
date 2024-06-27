aae78bed954c57fd2ea8352945b21265e3c6cfe7
ardalan zahiri <ardalanzahiri@gmail.com>
2024-06-27 22:24:21 +0330

    added bcrypt encoded pass to authorization server and higher access token ttl

--- banking-service/src/main/java/ir/snapppay/bankingservice/BankingServiceApplication.java
+++ banking-service/src/main/java/ir/snapppay/bankingservice/BankingServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankingServiceApplication {
-
+// todo : add secret header checker later in the end to prevent out of api gateway calling
     public static void main(String[] args) {
         SpringApplication.run(BankingServiceApplication.class, args);
     }
