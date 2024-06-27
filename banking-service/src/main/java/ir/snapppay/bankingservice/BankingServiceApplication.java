c0a5a4e33955f0cb7fae81813ce50f6335cf3bc2
ardalan zahiri <ardalanzahiri@gmail.com>
2024-06-27 17:57:06 +0330

    oauth2 authorization server finalized, also added resource server to banking service

---
+++ banking-service/src/main/java/ir/snapppay/bankingservice/BankingServiceApplication.java
@@ -0,0 +1,12 @@
+package ir.snapppay.bankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingServiceApplication.class, args);
+    }
+}
--- banking-service/src/main/java/ir/snapppay/bankingservice/controller/BankingServicesController.java
+++ banking-service/src/main/java/ir/snapppay/bankingservice/controller/BankingServicesController.java
@@ -1,5 +1,7 @@
 package ir.snapppay.bankingservice.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class BankingServicesController {
     //todo : dont forget to add swagger for api documenting as per requested later after all things done!
+
+    @PostMapping("/test")
+    @PreAuthorize("hasAnyAuthority('SCOPE_test.test')")
+    public String test() {
+        return "test";
+    }
 }
